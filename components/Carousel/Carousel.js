9fe8788c294b92b2861c414a2225c791e78c3482
JazmineMT <jazmineamcginnis@gmail.com>
2020-06-05 10:54:14 -0700

    carousel is somewhat functional

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -17,3 +17,96 @@
     <div class="right-button"> > </div>
   </div>
 */
+
+function carouselMaker (){
+
+const carousel = document.createElement('div')
+const leftButton = document.createElement('div')
+const image1 = document.createElement('img')
+const image2  = document.createElement('img')
+const image3 = document.createElement('img')
+const image4 = document.createElement('img')
+const rightButton = document.createElement('div')
+
+
+carousel.classList.add('carousel')
+leftButton.classList.add('left-button')
+rightButton.classList.add('right-button')
+
+
+carousel.appendChild(leftButton)
+carousel.appendChild(image1)
+carousel.appendChild(image2)
+carousel.appendChild(image3)
+carousel.appendChild(image4)
+carousel.appendChild(rightButton)
+
+image1.src = 'assets/carousel/computer.jpeg'
+image2.src = 'assets/carousel/mountains.jpeg'
+image3.src = 'assets/carousel/trees.jpeg'
+image4.src = 'assets/carousel/turntable.jpeg'
+
+
+leftButton.addEventListener ('click', event => { 
+image1.style.display ="block"
+image1.style.transition.opacity  = '2';
+
+setTimeout(function(){
+  image1.style.display= "";
+ 
+setTimeout(function(){
+  image2.style.display= "block";
+
+
+setTimeout(function(){
+  image2.style.display= "";
+
+
+ 
+setTimeout(function(){
+  image3.style.display= "block";
+
+  setTimeout(function(){
+    image3.style.display= "";
+
+
+    setTimeout(function(){
+      image4.style.display= "block";
+
+
+      setTimeout(function(){
+        image4.style.display= "";
+      }, 900);
+    }, 900);
+  }, 900);
+
+}, 900);
+}, 900);
+}, 900);
+
+}, 900);
+})
+
+
+rightButton.addEventListener ('click', event => { 
+  image2.style.display = "block"
+setTimeout(function(){
+  image2.style.display= "";
+}, 700);
+})
+
+
+
+// leftButton.addEventListener ('click', event => { image3.style.display ='block'})
+// leftButton.addEventListener ('click', event => { image4.style.display ='block'})
+// rightButton.addEventListener ('click', event => { image4.style.display ='none'})
+
+
+return carousel
+
+}
+
+const carouselContainer = document.querySelector(".carousel-container")
+const newCarousel = carouselMaker();
+carouselContainer.appendChild(newCarousel)
+
